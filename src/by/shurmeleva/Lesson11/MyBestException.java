2ea6c100fce2e00768e237570c39e484698240ef
AnnaShurmeleva25 <anna.shurmeleva@mail.ru>
2023-04-26 21:48:37 +0300

    Lesson 11

---
+++ src/by/shurmeleva/Lesson11/MyBestException.java
@@ -0,0 +1,16 @@
+package by.shurmeleva.Lesson11;
+
+public class MyBestException extends Exception {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public MyBestException(String message, int age) {
+        super(message);
+        this.age = age;
+
+    }
+
+}
